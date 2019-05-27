0beda178c250d0e8435c82a1b66926f1b39e3b93
Arkadious <51068504+Arkadious@users.noreply.github.com>
2019-05-27 16:23:02 +1200

    test

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <mbed.h>
+#include <string.h>
+
+int main(void)
+{
+
+  // put your setup code here, to run once:
+
+  while(1)
+  {
+    // put your main code here, to run repeatedly:
+  }
+}
